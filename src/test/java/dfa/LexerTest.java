ffd4ea3c733eeeb9d9732cb682c5daac4fc77b44
jeff <jeff@urbanlogiq.com>
2025-07-29 14:02:02 -0700

    More work on context; unit tests

--- src/test/java/dfa/LexerTest.java
+++ src/test/java/dfa/LexerTest.java
@@ -3,7 +3,10 @@ package dfa;
 import js.base.Pair;
 import js.data.ByteArray;
 import js.data.DataUtil;
+import js.file.FileException;
+import js.file.Files;
 import js.json.JSList;
+import js.parsing.Scanner;
 import js.testutil.MyTestCase;
 
 import static js.base.Tools.*;
@@ -225,6 +228,13 @@ public class LexerTest extends MyTestCase {
 
 
   @Test
+  public void code1() {
+    rv();
+    proc();
+  }
+
+
+  @Test
   public void context() {
     rv();
     tokens("{\"graph\":[0,5,3,9,2,12,2,32,1,115,0,3,9,2,12,2,32,1,115,0,3,9,2,12,2,32,1,115,0,1,120,1,108,0,1,47,1,39,0,0,1,1,42,1,46,0,0,3,2,1,41,43,85,46,0,2,1,41,43,85,46,0,1,42,1,67,0,0,5,3,1,41,43,4,48,80,46,0,3,1,41,43,4,48,80,46,0,3,1,41,43,4,48,80,46,0,1,42,1,67,0,1,47,1,106,0,3,0,2,1,1,120,1,108,0,1,3,3,9,2,12,2,32,1,115,0,3,9,2,12,2,32,1,115,0,3,9,2,12,2,32,1,115,0],\"token_names\":\"WS CODE COMMENT\",\"version\":\"$2\"}");
@@ -315,4 +325,105 @@ public class LexerTest extends MyTestCase {
   private Integer mSkip;
   private boolean mAllowUnknown;
 
+
+  private void proc() {
+    proc(null);
+  }
+
+  private boolean mDisallowUnknown;
+
+  private void disallowUnknown() {
+    mDisallowUnknown = true;
+  }
+
+
+  private void proc(String sampleText) {
+
+    if (sampleText == null) {
+      String resourceName = testName() + ".txt";
+      sampleText = Files.readString(this.getClass(), resourceName);
+    }
+    var source = sampleText;
+
+    DFACompiler c = new DFACompiler();
+    c.setVerbose(verbose());
+    var jsonDFA = DFA.parse("{\"graph\":[0,16,1,125,1,-114,1,1,123,1,-116,1,1,116,1,117,1,1,110,1,94,1,1," +
+        "102,1,64,1,1,93,1,62,1,1,91,1,60,1,1,58,1,58,1,1,49,9,39,1,1,48,1,-24,0,1,47,1,-73,0,1,45,1,-85,0,1,44," +
+        "1,-87,0,1,35,1,-94,0,1,34,1,97,0,3,9,2,12,2,32,1,86,0,1,1,3,9,2,12,2,32,1,86,0,0,3,3,32,2,35,57,93,35,97," +
+        "0,1,92,1,120,0,1,34,1,118,0,11,0,0,3,3,32,2,35,57,93,35,97,0,1,92,1,120,0,1,34,1,-115,0,11,3,3,32,2," +
+        "35,57,93,35,97,0,1,92,1,120,0,1,34,1,118,0,1,1,1,32,96,-94,0,9,0,0,2,1,49,9,39,1,1,48,1,-24,0,0,2,1," +
+        "42,1,-61,0,1,47,1,-94,0,0,2,2,1,41,43,85,-61,0,1,42,1,-47,0,0,3,3,1,41,43,4,48,80,-61,0,1,42,1,-47," +
+        "0,1,47,1,-26,0,1,0,12,2,2,69,1,101,1,11,1,1,46,1,-10,0,0,1,1,48,10,-3,0,12,2,2,69,1,101,1,11,1,1,48," +
+        "10,-3,0,0,2,1,48,10,32,1,2,43,1,45,1,25,1,0,1,1,48,10,32,1,12,1,1,48,10,32,1,12,3,1,48,10,39,1,2," +
+        "69,1,101,1,11,1,1,46,1,-10,0,10,0,2,0,3,0,0,1,1,97,1,71,1,0,1,1,108,1,78,1,0,1,1,115,1,85,1,0,1,1," +
+        "101,1,92,1,5,0,0,1,1,117,1,101,1,0,1,1,108,1,108,1,0,1,1,108,1,115,1,6,0,0,1,1,114,1,124,1,0,1,1," +
+        "117,1,-125,1,0,1,1,101,1,-118,1,4,0,7,0,8,0],\"token_names\":\"WS BROP BRCL TRUE FALSE NULL CBROP" +
+        " CBRCL COMMA COLON STRING NUMBER\",\"version\":\"$2\"}");
+
+
+    {
+      StringBuilder sb = new StringBuilder();
+      var s = new Lexer(jsonDFA);
+      pr("text:",source);
+      s.withText(source);
+      if (!mDisallowUnknown)
+        s.withAcceptUnknownTokens();
+      s.withSkipId(0);
+      s.setVerbose(verbose());
+      s.start();
+
+      List<Lexeme> tok = arrayList();
+      while (s.hasNext()) {
+        var t = s.read();
+        tok.add(t);
+      }
+
+      for (var tk : tok) {
+        var cts = tk.plotWithinContext();
+        sb.append("-----------------------------------------------------------------------------------\n");
+        sb.append(cts);
+      }
+      sb.append("-----------------------------------------------------------------------------------\n");
+
+      String result = sb.toString();
+      log("Parsed tokens:", INDENT, result);
+      files().writeString(generatedFile("tokens.txt"), result);
+    }
+
+    assertGenerated();
+  }
+
+
+  private String testName() {
+    parseName();
+    return mTestName;
+  }
+
+  private int testVersion() {
+    parseName();
+    return mVersion;
+  }
+
+  private void parseName() {
+    // If unit test name is xxxx123, extract the suffix 123 as the version for the input
+    if (mTestName == null) {
+      String nm = name();
+      String testName = nm;
+      int j = nm.length();
+      while (true) {
+        char c = nm.charAt(j - 1);
+        if (!(c >= '0' && c <= '9'))
+          break;
+        j--;
+      }
+      if (j < nm.length()) {
+        mVersion = Integer.parseInt(nm.substring(j));
+        testName = nm.substring(0, j);
+      }
+      mTestName = testName;
+    }
+  }
+
+  private String mTestName;
+  private int mVersion = -1;
 }
